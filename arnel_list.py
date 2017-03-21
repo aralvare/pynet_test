299502dc8234ab1a91e87857a4195c11d9ca2227
aralvare <arnelalvarez@hotmail.com>
2017-03-20 17:12:57 -0700

    Update arnel_list.py

--- arnel_list.py
+++ arnel_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-my_list = 1,2,3,4,5
-my_list.append(6)
-my_list.append(7)
+#!/usr/bin/env python
+my_list = ['Uber', 10, 22.0, 'SF', 15]
 print my_list
-
-my_list.pop(0)
-print a_list
-
-len(my_list)
-my_list.sort()
-
+new_list = my_list[:]
+new_list.extend([12, 'test123'])
+print new_list
+new_list.pop(0)
+print new_list
+new_list.sort()
+print new_list 
